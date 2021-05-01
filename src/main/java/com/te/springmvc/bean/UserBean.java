8603e4f5dc9306e757af3b91d33e82864b0dc067
= <=>
2021-05-01 14:52:17 +0530

    added spring-mvc project

---
+++ src/main/java/com/te/springmvc/bean/UserBean.java
@@ -0,0 +1,19 @@
+package com.te.springmvc.bean;
+
+public class UserBean {
+	private String user;
+	private String pwd;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
